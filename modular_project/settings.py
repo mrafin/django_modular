ae27503525b6122d2daf0de9d6be6f8e51963a64
mrafin <55218099+mrafin@users.noreply.github.com>
2025-04-23 16:58:56 +0700

    turn of debug

--- modular_project/settings.py
+++ modular_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(k9s5ody=ly(4fbw1j$q(eml@x-&x3tr#pb&w8+*l@*%znfnlw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
